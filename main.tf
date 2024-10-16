a1dc99f9d322d90597a781883e58dc9a816979d6
Saquib2507 <bagwansaquib24@gmail.com>
2024-10-16 16:09:36 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -31,6 +31,29 @@ resource "aws_subnet" "main" {
   }
 }
 
+# creating 4EC2 instance 
+/*resource "aws_instance" "main" {
+  ami = "ami-0845068028e672a07"
+  instance_type = "t2.micro"
+  count = 4
+  subnet_id = element(aws_subnet.main[*].id, count.index % length(aws_subnet.main))
+  tags = {
+    Name = "${local.project}-instance-${count.index}"
+  }
+}*/
+
+
+resource "aws_instance" "main" {
+  count = length(var.ec2_config)
+  ami = var.ec2_config[count.index].ami
+  instance_type = var.ec2_config[count.index].instance_type
+  subnet_id = element(aws_subnet.main[*].id, count.index % length(aws_subnet.main))
+
+  tags = {
+    Name = "${local.project}-instance-${count.index}"
+  }
+}
+
 output "aws_subnet_id" {
   value = aws_subnet.main[0].id
 }
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+ec2_config = [ {
+  ami = "ubuntu"
+  instance_type = "t2.micro"
+}, {
+    ami = "amazon"
+    instance_type = "t2.micro"
+} ]
